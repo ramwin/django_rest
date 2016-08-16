39e0781705f75103503010af62ec87af3b7d7160
Xiang Wang <ubuntu@mac.cn>
2016-08-16 11:45:02 +0800

    创建snippets和登陆的用户绑定起来了。

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -2,16 +2,21 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from snippets.serializers import UserSerializer
 from rest_framework import generics
+from rest_framework import permissions
 from django.contrib.auth.models import User
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     serializer_class = SnippetSerializer
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     serializer_class = SnippetSerializer
 
 

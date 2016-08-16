80984b245067a083c1a68fa13a6f30ae57ba6a7b
Xiang Wang <ubuntu@mac.cn>
2016-08-16 11:37:06 +0800

    添加了用户

--- tutorial/test.sh
+++ tutorial/test.sh
@@ -5,3 +5,6 @@ http http://localhost:8000/snippets/
 http --form POST http://localhost:8000/snippets/ code="$(date +%y-%m-%d\ %H:%M:%S)"
 http --json DELETE http://localhost:8000/snippets/1/
 http --json PUT http://localhost:8000/snippets/2/ code="$(date +%y-%m-%d\ %H:%M:%S)"
+
+http http://localhost:8000/users/
+http http://localhost:8000/users/1/

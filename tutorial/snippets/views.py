b4ff25d266ed4ab49c2ce334c12dd008ed0c3b54
Xiang Wang <ubuntu@mac.cn>
2016-08-16 11:55:22 +0800

    API的删除修改需要用户进行认证

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -3,6 +3,7 @@ from snippets.serializers import SnippetSerializer
 from snippets.serializers import UserSerializer
 from rest_framework import generics
 from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
 from django.contrib.auth.models import User
 
 
@@ -15,6 +16,8 @@ class SnippetList(generics.ListCreateAPIView):
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly,)
     queryset = Snippet.objects.all()
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     serializer_class = SnippetSerializer

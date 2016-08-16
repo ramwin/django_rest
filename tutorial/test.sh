51d9c3f38aec082fe097e4eb8859081b851a987b
Xiang Wang <ubuntu@mac.cn>
2016-08-16 11:27:43 +0800

    测试的脚本

---
+++ tutorial/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Xiang Wang @ 2016-08-16 11:20:00
+
+http http://localhost:8000/snippets/
+http --form POST http://localhost:8000/snippets/ code="$(date +%y-%m-%d\ %H:%M:%S)"
+http --json DELETE http://localhost:8000/snippets/1/
+http --json PUT http://localhost:8000/snippets/2/ code="$(date +%y-%m-%d\ %H:%M:%S)"

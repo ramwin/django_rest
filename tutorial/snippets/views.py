72c6071e78ec8c9134373e26283d0cdcd7f48615
Xiang Wang <ubuntu@mac.cn>
2016-08-16 12:35:51 +0800

    使用了api的url来定位各种model的关系

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -5,6 +5,10 @@ from rest_framework import generics
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
 from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -31,3 +35,23 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+
+from rest_framework import renderers
+from rest_framework.response import Response
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'snippets.apps.SnippetsConfig',
 ]
 
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 10
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
